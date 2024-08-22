a188a00274c8dad3b6873a40b8a07d886e50c7ac
Kacper <informatyka402@gmail.com>
2024-08-23 01:53:55 +0200

    added categories in nav

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,26 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 
-
 import './App.css';
+import oslo from '../src/assets/img/72 hours in Oslo, Norway.jfif';
 
-
-function Top(){
-  return(
-      <li>
-          <h3></h3>
-          <h3></h3>
-          <h3></h3>
-          
-          
-      </li>
-  )
-  }
+function Top() {
+	return (
+		<li id='list'>
+			<h3>Miejsca wartę uwagi</h3>
+			<h3>Wskazówki i porady</h3>
+			<h3>Ruter #</h3>
+			<h3>Kontakt</h3>
+			<h3 id='black'>English  or </h3>
+		</li>
+	);
+}
 
 function App() {
 	return (
 		<div>
 			<section id='nav'>
-				<main>
-					<Top 
-            
-          />
-				</main>
+				<Top />
 			</section>
 		</div>
 	);
