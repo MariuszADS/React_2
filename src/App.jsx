7320b085ec14c3f41a0cf60ec36e33abe48fdef2
Kacper <informatyka402@gmail.com>
2024-08-28 02:35:56 +0200

    added shadow on main img oslo s but is not working

--- src/App.jsx
+++ src/App.jsx
@@ -21,16 +21,18 @@ function BackGround() {
 function App() {
 	return (
 		<div>
-			<BackGround />
-			<section id='nav'>
-				<li id='list'>
-					<Button_nav>Miejsca wartę uwagi</Button_nav>
-					<Button_nav>Wskazówki i porady</Button_nav>
-					<Button_nav>Ruter #</Button_nav>
-					<Button_nav>Kontakt</Button_nav>
-					<Button_nav id='black'>English or </Button_nav>
-				</li>
-			</section>
+			<div id='shadow'>
+				<BackGround />
+				<section id='nav'>
+					<li id='list'>
+						<Button_nav>Miejsca wartę uwagi</Button_nav>
+						<Button_nav>Wskazówki i porady</Button_nav>
+						<Button_nav>Ruter #</Button_nav>
+						<Button_nav>Kontakt</Button_nav>
+						<Button_nav id='black'>English or </Button_nav>
+					</li>
+				</section>
+			</div>
 		</div>
 	);
 }
