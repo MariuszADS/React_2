e6b0d0d69e249f0dbb3f650b0d0ecdd189940670
Kacper <informatyka402@gmail.com>
2024-08-29 14:26:10 +0200

    btn moved in BackGround folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,27 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 
+import BackGround from './BackGround/BackGround' 
 import './App.css';
-import oslo from '../src/assets/img/72 hours in Oslo, Norway.jfif';
-// import { DATA_FROM } from './data_with';
+
 
 function Button_nav({ children }) {
 	return <button>{children}</button>;
 }
 
-function BackGround() {
-	return (
-		<div id='Welcome_web_page'>
-			<img src={oslo} />
-		</div>
-	);
-}
 
 function App() {
 	return (
 		<div>
-			<div id='shadow'>
-				<BackGround />
-				<section id='nav'>
-					<li id='list'>
-						<Button_nav>Miejsca wartę uwagi</Button_nav>
-						<Button_nav>Wskazówki i porady</Button_nav>
-						<Button_nav>Ruter #</Button_nav>
-						<Button_nav>Kontakt</Button_nav>
-						<Button_nav id='black'>English or </Button_nav>
-					</li>
-				</section>
-			</div>
+			<BackGround />
+
+			<section id='nav'>
+				<li id='list'>
+					<Button_nav>Miejsca wartę uwagi</Button_nav>
+					<Button_nav>Wskazówki i porady</Button_nav>
+					<Button_nav>Ruter #</Button_nav>
+					<Button_nav>Kontakt</Button_nav>
+					<Button_nav>English or </Button_nav>
+				</li>
+			</section>
 		</div>
 	);
 }
