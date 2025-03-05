38d3ff0ff8e61a6adf6c2aee82cfac08d9b6aefa
Kacper <informatyka402@gmail.com>
2025-03-05 20:03:45 +0100

    nothing had changed

---
+++ src/Components/Miejsca_context.jsx
@@ -0,0 +1,17 @@
+export default function Miejsca_context() {
+    
+    // function moveImages(id){
+    //     if(null)
+    // }
+
+	return <div>
+        <div className="flex_container">
+            <img className='img_carusel'>
+                <button className="img_carusel_arrow">↗️</button>
+            </img>
+            <h2 className='img_header'>Consectetur</h2>
+            <p className="img_paragraph">Labore adipisicing irure ipsum Lorem ex ad irure mollit. Anim anim elit non anim pariatur. Nostrud sunt non exercitation ex deserunt veniam cillum anim ipsum labore. Quis duis elit irure aliqua cupidatat reprehenderit anim voluptate dolor esse esse ut irure incididunt.</p>
+        </div>
+    </div>;
+}
+
--- src/routers/Miejsca_warte_uwagi.jsx
+++ src/routers/Miejsca_warte_uwagi.jsx
@@ -1,7 +1,7 @@
 import SmallLogo from '../Components/SmallLogo';
 import Navbar_white from '../Components/Navbar_white';
 import Footer from '../Components/Footer';
-
+// import Miejsca_context from '../Components/Miejsca_context';
 
 
 const Miejsca_warte_uwagi = () => {
@@ -9,6 +9,7 @@ const Miejsca_warte_uwagi = () => {
 		<div className='linear-gradient'>
 			<SmallLogo/>
 			<Navbar_white/>
+			{/* <Miejsca_context/> */}
 			<Footer />
 		</div>
 	);
